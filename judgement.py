24ef4b450c1bd3f8eba9adb08b8af4bcaac62381
Hackbright Student <user@mini-003.(none)>
2014-03-11 18:07:19 -0700

    Pt2 almost almost done

--- judgement.py
+++ judgement.py
@@ -3,18 +3,29 @@ import model
 app = Flask(__name__)
 app.secret_key = "maryhasalittlelamb?!....."
 
-# @app.route("/user_list")
-# def index():
-#     user_list = model.session.query(model.User).limit(5).all()
-#     print user_list
-#     return render_template("user_list.html", users=user_list)
+@app.route("/my_rating")
+def my_rating():
+    return render_template("my_rating.html")
+
+@app.route("/user/<user_id>")
+def user_movies_ratings(user_id):
+    movie_ids = model.session.query(model.Rating).filter_by(user_id=user_id)
+    movies = []
+
+    for movie_id in movie_ids:
+        u = movie_id.movie
+        movies.append({"name":u.name, "rating":movie_id.rating})     
+
+    return render_template("search_page.html", movies=movies)
+
 @app.route("/")
 def index():
     return render_template("new_user_signup.html")
 
-@app.route("/search_page")
-def search_page():
-    return render_template("search_page.html")
+@app.route("/user_list")
+def user_list():
+    user_list = model.session.query(model.User).limit(10).all()
+    return render_template("user_list.html", users=user_list)
 
 @app.route("/existing_user_login", methods=["POST"])
 def existing_user_login():
@@ -23,11 +34,11 @@ def existing_user_login():
     try:
         existing = model.session.query(model.User).filter_by(email=email, password=password).one()
     except:
-        flash("Invalid username or password!", "Error")
+        flash("Invalid username or password!")
         return redirect(url_for("index"))
 
     #model.session['user_id'] = existing.id
-    return redirect(url_for("search_page"))
+    return redirect(url_for("user_list"))
 
 
 @app.route("/new_user_signup", methods=["POST"])
@@ -39,18 +50,19 @@ def new_user_signup():
     zipcode = request.form.get("zipcode")
 #Verification if user already exists
     existing = model.session.query(model.User).filter_by(email=email).first()
+
     if existing:
-        flash("This email already exists,Please select another one!", "Error")
+        flash("This email already exists,Please select another one!")
         return redirect(url_for("index")) #redirect back to sign up page
     elif password != password_ver:
-        flash("Your password do not match", "Error")
+        flash("Your password do not match")
         return redirect(url_for("index")) #redirect back to sign up page
     else:
         new_user = model.User(email=email, password=password, age=age, zipcode=zipcode)
         model.session.add(new_user)
         model.session.commit()
       #  model.session['user_id'] = new_user.id
-        return redirect(url_for("search_page")) 
+        return redirect(url_for("user_list")) 
  
 
 if __name__ == "__main__":
